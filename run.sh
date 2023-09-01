e96f461dba613fec44db7341a3baa6bb0a776a7c
Thomas R. Dial <dialtr@gmail.com>
2023-09-01 18:09:54 -0400

    added run.sh, a smoke test shell script. Assumes a valid in iso/

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ISO_PATH="./iso/akai-sample-cd.iso"
+BIN_PATH="./cmake-build-debug/akaiutil"
+
+# A simple smoke test to ensure that akaiutil is still working after refactors.
+
+if [ ! -f "${ISO_PATH}" ]; then
+  echo "Error: Expecting '${ISO_PATH}' ISO file to exist."
+	exit 1
+fi
+
+if [ ! -f "${BIN_PATH}" ]; then
+	echo "Error: Debug target '${BIN_PATH}' not found."
+	echo "Did you forget to build the project?"
+	exit 1
+fi
+
+# Run akaiutil, specifying the disc to open.
+# TODO(tdial): Enhance akaiutil to run smoke tests and exit with 0 or 1
+"${BIN_PATH}" -f "${ISO_PATH}"
+
+# Did we exit with zero status?
+EXIT_CODE="$?"
+if [ "${EXIT_CODE}" -ne "0" ]; then
+	echo "run.sh: test failed"
+else
+	echo "run.sh: test passed"
+fi
+
